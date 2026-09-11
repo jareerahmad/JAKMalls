b0a890ba76f8caab69f43907fd00c2a306768c80
jareerahmad <jareerahmad42@gmail.com>
2026-09-10 19:57:34 -0700

    Prepare backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
